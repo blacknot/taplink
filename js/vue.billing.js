eac7306e426ba6f2ea4673e8e34ff50c13e2b4cc
Konstantin <blacknot@gmail.com>
2020-05-14 20:26:54 +0300

    1.0.0

---
+++ js/vue.billing.js
@@ -0,0 +1,1 @@
+window.$app.defineComponent("billing","vue-billing-index",{data:function(){return{}},created:function(){this.fetchData(),alert(1)},methods:{fetchData:function(){}},template:"<div> billing </div>"}),window.$app.defineModule("billing",[]);
